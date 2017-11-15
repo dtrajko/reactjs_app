a52ae833edc7b68817662aa3ad2d4203d18a5501
Dejan Trajkovic <dtrajko@gmail.com>
2017-11-15 09:01:37 -0500

    Form validation with PropTypes

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ProjectItem from './ProjectItem';
+import PropTypes from 'prop-types'; // ES6
 
 class Projects extends Component {
     
@@ -25,6 +26,11 @@ class Projects extends Component {
         </div>
       );
     }
+};
+
+Projects.propTypes = {
+    projects: PropTypes.array,
+    onDelete: PropTypes.func
 };
 
 export default Projects;
